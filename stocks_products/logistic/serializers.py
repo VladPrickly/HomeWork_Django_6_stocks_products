86832b11830a603e52c6b5502be9325bb09305c8
Vladislav <vladislav.goltsev@gmail.com>
2025-12-24 19:49:56 +0300

    create serializers

--- stocks_products/logistic/serializers.py
+++ stocks_products/logistic/serializers.py
@@ -1,25 +1,20 @@
 from rest_framework import serializers
 
-from stocks_products.logistic.models import Product
+from models import Product, StockProduct, Stock
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = __all__
-    # настройте сериализатор для продукта
-    pass
-
-
-class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    pass
-
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
 
     # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['address', 'positions']
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
@@ -31,12 +26,17 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+        for position in positions:
+            position['stock'] = stock
+            StockProduct.objects.create(**position)
 
         return stock
 
     def update(self, instance, validated_data):
+
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
+
 
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
@@ -44,5 +44,9 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+        for position in positions:
+            StockProduct.objects.update_or_create(stock=stock, product=position.get('product'),
+                                                  defaults={'quantity': position.get('quantity'),
+                                                            'price': position.get('price')})
 
         return stock
--- stocks_products/stocks_products/settings.py
+++ stocks_products/stocks_products/settings.py
@@ -136,6 +136,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
+    'SEARCH_PARAM': 'q',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2
+    'PAGE_SIZE': 10,
 }
